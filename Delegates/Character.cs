2ea26e4d6e44e152b092b68a0269b83e543e817b
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-03-25 18:21:56 +0600

    Init

---
+++ Delegates/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    [Serializable]
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Mass { get; set; }
+        public string HairColor { get; set; }
+        public string SkinColor { get; set; }
+        public string EyeColor { get; set; }
+        public string BirthYear { get; set; }
+        public string Gender { get; set; }
+        public string Homeworld { get; set; }
+        public string[] Films { get; set; }
+        public string[] Species { get; set; }
+        public string[] Vehicles { get; set; }
+        public string[] Starships { get; set; }
+        public string Created { get; set; }
+        public string Edited { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json.Serialization;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите идентификатор персонажа: ");
+            int identifier = int.Parse(Console.ReadLine());
+            string urlIdentifier = $"https://swapi.co/api/people/{identifier}/";
+
+            string filePath = "characters.xml";
+
+            Character character = new Character();
+
+            using (FileStream stream = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(filePath);
+                XmlElement rootElement = xmlDocument.DocumentElement;
+
+                for (int i = 0; i < rootElement.GetElementsByTagName("results").Count; i++)
+                {
+                    var urlElement = rootElement.GetElementsByTagName("results")[0] as XmlElement;
+                    urlElement = urlElement.GetElementsByTagName("url")[i] as XmlElement;
+                    if (urlElement.InnerText == urlIdentifier)
+                    {
+                        XmlSerializer serializer = new XmlSerializer(typeof(Character));
+                        character = serializer.Deserialize(stream, );
+
+                    }
+                }
+
+
+            }
+        }
+    }
+}
